15607e569bc9cd51d341e93a1d3a8053dbca4320
Kuzkin <heartsong_@mail.ru>
2024-08-12 04:07:25 +0500

    add new file cipher_schemas.py
    working with the cipher

---
+++ cipher/cipher_schemas.py
@@ -0,0 +1,55 @@
+from fastapi import HTTPException
+from pydantic import BaseModel
+
+
+class CipherMessageSchema(BaseModel):
+    """
+    Схема для зашифрованного сообщения
+    """
+    cipher_message: str
+    pass_phrase: str
+
+
+class EncodingSchema(BaseModel):
+    """
+    Схема для зашифрованного сообщения с ключом
+    """
+    pass_phrase: str
+
+    @property
+    def encoding_key(self):
+        if self.pass_phrase is None:
+            raise HTTPException(status_code=403, detail='Укажите проверочное слово')
+
+
+class CipherOutput(BaseModel):
+    """
+    Схема для ответа с зашифрованным сообщением
+    """
+    cipher_message: str
+    pass_phrase: str
+    key_cipher: str
+    url: str
+
+    class Config:
+        """
+        Конфигурационный класс для настройки ORM.
+        Позволяет использовать ORM-объекты для сериализации.
+        """
+        orm_mode = True
+
+
+class MessageOutSchema(BaseModel):
+    """
+    Схема для ответа с зашифрованным сообщением и полями id, email и password.
+    """
+    cipher_message: str
+    pass_phrase: str
+    key_cipher: str
+
+    class Config:
+        """
+        Конфигурационный класс для настройки ORM.
+        Позволяет использовать ORM-объекты для сериализации.
+        """
+        orm_mode = True
