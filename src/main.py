fa578ce79d2a907fab498a915601b2cdcd8195f2
Kuzkin <heartsong_@mail.ru>
2024-08-14 22:56:56 +0500

    add changed main.py file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from authentication import auth_routers
-from cipher import cipher_routers
+from src.authentication import auth_routers
+from src.cipher import cipher_routers
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
-from users import user_routers
+from src.users import user_routers
+
 
 app = FastAPI(title="key_manager", version="0.1.0")
+
+add_pagination(app)
 
 app.include_router(user_routers.router)
 app.include_router(cipher_routers.router)
