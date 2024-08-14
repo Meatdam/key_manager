6cf104bde126897ffc0127ceb8b14ff24acef8af
Kuzkin <heartsong_@mail.ru>
2024-08-14 22:55:09 +0500

    add changed cipher_services.py file

--- src/cipher/cipher_services.py
+++ src/cipher/cipher_services.py
@@ -1,10 +1,13 @@
 from cryptography.fernet import Fernet
+from fastapi_pagination import Params
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from fastapi import HTTPException
+from fastapi_pagination.ext.sqlalchemy import paginate
 
-from database.db import Cipher
-from cipher.cipher_schemas import CipherMessageSchema
+from src.models.models import Cipher
+
+from src.cipher.cipher_schemas import CipherMessageSchema
 
 
 key = Fernet.generate_key()
@@ -13,7 +16,7 @@ fernet = Fernet(key)
 
 async def create_cipher(message: CipherMessageSchema, user_id: int, db: AsyncSession):
     """
-    Создание нового зашифрованного сообщения и сохранение в БД
+    Create a new encrypted message and save it to the database
     """
     messages = message.cipher_message
     cipher_message = fernet.encrypt(messages.encode())
@@ -21,7 +24,7 @@ async def create_cipher(message: CipherMessageSchema, user_id: int, db: AsyncSes
     cipher_phrase = fernet.encrypt(pass_phrase.encode())
 
     load_data = Cipher(key_cipher=str(key), cipher_message=str(cipher_message), pass_phrase=str(cipher_phrase),
-                       user_id=user_id, url=f"http://127.0.0.1:8000/key/{cipher_message}",
+                       user_id=user_id, url=f"http://127.0.0.1:8000/secrets/{cipher_message}",
                        life_cipher=message.life_cipher)
     db.add(load_data)
     await db.commit()
@@ -29,19 +32,21 @@ async def create_cipher(message: CipherMessageSchema, user_id: int, db: AsyncSes
     return load_data
 
 
-async def get_cipher_list(current_id: int, db: AsyncSession):
+async def get_cipher_list(current_id: int, db: AsyncSession,
+                          params: Params = Params()):
     """
-    Получить список зашифрованных сообщений
+    Get list of encrypted messages
     """
-    query = await db.execute(select(Cipher).where(Cipher.user_id == current_id))
-    db_message = query.scalars().all()
+    # query = await db.execute(select(Cipher).where(Cipher.user_id == current_id))
+    # db_message = query.scalars().all()
+    query = select(Cipher).where(Cipher.user_id == current_id)
 
-    return db_message
+    return await paginate(db, query, params)
 
 
 async def get_cipher_by_id(id_message: int, current_id: int, db: AsyncSession) -> Cipher:
     """
-    Получить зашифрованное сообщение по его id
+    Get encrypted message by its id
     """
     query = await db.execute(select(Cipher).where(Cipher.id == id_message))
     db_message = query.scalars().first()
@@ -52,7 +57,7 @@ async def get_cipher_by_id(id_message: int, current_id: int, db: AsyncSession) -
 
 async def decrypt_cipher(url: str, pass_phrase: str, db: AsyncSession):
     """
-    Расшифровка зашифрованного сообщения и удаление из БД
+    Decrypting an encrypted message and deleting it from the database
     """
     message = await db.execute(select(Cipher).where(Cipher.cipher_message == url))
     db_message = message.scalars().first()
